d085b1b9b2545f48d0b24f0e4f42de9962f50e21
Akhileswar <akhileswarsathivada@gmail.com>
2024-05-18 13:57:30 +0530

    commit 2

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,18 +3,20 @@ import styles from "./Navbar.module.css";
 import logo from "../../../public/assests/logo.png";
 const Navbar = () => {
   return (
-    <div className={styles.navbar}>
-      <div className={styles.logo}>
-        <Image src={logo} alt="logo" />
-      </div>
-      <div className={styles.navLinks}>
-        <a href="/">Home</a>
-        <a href="/applications">Apps</a>
-        <a href="/contact-us">Contact Us</a>
-        <a href="/about">About Us</a>
-        <a href="/teams">Team</a>
-        <a href="/our-work">Our Works</a>
-        <a href="awards">Awards and Recognitions</a>
+    <div className={styles.mainContainer}>
+      <div className={styles.navbar}>
+        <div className={styles.logo}>
+          <Image src={logo} alt="logo" />
+        </div>
+        <div className={styles.navLinks}>
+          <a href="/">Home</a>
+          <a href="/applications">Apps</a>
+          <a href="/contact-us">Contact Us</a>
+          <a href="/about">About Us</a>
+          <a href="/teams">Team</a>
+          <a href="/our-work">Our Works</a>
+          <a href="awards">Awards and Recognitions</a>
+        </div>
       </div>
     </div>
   );
